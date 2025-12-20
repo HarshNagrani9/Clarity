32d91266fb680d17f338e602c3f7019dbfdf2685
Harsh Nagrani <harshnagrani0910@gmail.com>
2025-12-20 16:07:31 +0530

    Added my own favicon

---
+++ app/icon.tsx
@@ -0,0 +1,52 @@
+
+import { ImageResponse } from 'next/og';
+
+// Route segment config
+export const runtime = 'edge';
+
+// Image metadata
+export const size = {
+    width: 32,
+    height: 32,
+};
+export const contentType = 'image/png';
+
+// Image generation
+export default function Icon() {
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 20,
+                    background: '#a3e635', // lime-400
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'black',
+                    border: '2px solid black',
+                    borderRadius: '4px',
+                }}
+            >
+                <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
+                </svg>
+            </div>
+        ),
+        // ImageResponse options
+        {
+            ...size,
+        }
+    );
+}
